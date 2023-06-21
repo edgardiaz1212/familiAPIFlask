b9af912ef7d1dd9aeb25fed0ddaeae88aaaee6a5
Edgar Diaz <ed35376@gmail.com>
2023-06-20 21:52:22 -0400

    app done

--- src/app.py
+++ src/app.py
@@ -36,7 +36,39 @@ def handle_hello():
     }
 
 
-    return jsonify(response_body), 200
+    return jsonify(response_body), 200 if members else 400
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        response_body={
+            "id":member['id'],
+            "first_name":member['first_name'],
+            "age":member['age'],
+            "lucky_numbers":member['lucky_numbers']
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify({"message":"Member not found"}), 400
+
+@app.route('/member/', methods =['POST'])
+def add_member():
+    try:
+        data=request.get_json()
+        jackson_family.add_member(data)
+        return jsonify({"message":"member added"}),200
+    except:
+        return jsonify({"message":"Not added"}), 400
+
+@app.route('/member/<int:member_id>' , methods=['DELETE'])
+def delete_member(member_id):
+    try:
+        jackson_family.delete_member(member_id)
+        return jsonify({"message":"member deleted"}) , 200
+
+    except:
+        return jsonify({"message":"failed"}) , 400    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
