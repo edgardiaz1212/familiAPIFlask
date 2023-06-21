c747b9feb6f46d555a08ffd30a21c188f822a67f
Edgar Diaz <ed35376@gmail.com>
2023-06-20 21:18:48 -0400

    datastructures

--- src/datastructures.py
+++ src/datastructures.py
@@ -21,15 +21,21 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member_id=self._generateId()
+        member['id']=member_id
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if (member["id"]==id):
+                self._members.remove(member)
+                break
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if (member["id"] == id):
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
